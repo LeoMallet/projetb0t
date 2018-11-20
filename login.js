648c2cdf1a781502da8bc840640dfc9abd3d65eb
Léo <leomalletdida@gmail.com>
2018-11-20 14:00:12 +0100

    Fin de la fonctionnalité de login

--- login.js
+++ login.js
@@ -21,6 +21,22 @@ else {
 // Pas mal mais en utilisant JQuery, tu peux aller plus vite :
 
 // $([SELECTEUR_CSS]) te donne un ou les X éléments qui match le sélecteur CSS donné, ici je choppe l'élément avec l'id connexion
+//Quand on click sur le bouton "connexion"
 $('#connexion').on('click', () => {
-    alert('JE SUIS CLICKE LOL');
+    // $(...).on('nomDeLEvenement', function) => ajouter un "event handler". On réagit à un événement (click, keypress, mouseenter etc etc)
+
+    // On récupère les informations
+    const login = $('#login').val();
+    const mdp = $('#password').val();
+
+    console.log('login', login);
+    console.log('mdp', mdp);
+
+    //comparaison de l'input avec les bon credentials
+    if (login === 'admin' && mdp === 'admin') {
+        window.location.href = "http://www.kana.fr/top-15-des-waifus/"; //lol
+    }
+    else {
+        window.alert("NAN C'EST PAS CA");;
+    }
 });
