5760cd0b55422854ec9515b4ee4a27ffdff532c2
Léo <leomalletdida@gmail.com>
2018-11-21 15:57:13 +0100

    Fin du login

--- login.js
+++ login.js
@@ -1,26 +1,8 @@
-
-/*document.getElementById("connect").onclick = function () {
-// récupération de l'input de l'utilisateur
-var login = document.getElementById("login");
-var mdp = document.getElementById("password");
-
-//comparaison de l'input avec les bon credentials
-if (login == 'admin' && mdp == 'admin') {
-    return window.location.href = "http://www.kana.fr/top-15-des-waifus/";;
-}
-else {
-    return window.alert("NAN C'EST PAS CA");;
-}
-// si vrai alors je me connecte
-
-
-};*/
-
-
-
-// Pas mal mais en utilisant JQuery, tu peux aller plus vite :
-
 // $([SELECTEUR_CSS]) te donne un ou les X éléments qui match le sélecteur CSS donné, ici je choppe l'élément avec l'id connexion
+
+//1. Connexion
+
+
 //Quand on click sur le bouton "connexion"
 $('#connexion').on('click', () => {
     // $(...).on('nomDeLEvenement', function) => ajouter un "event handler". On réagit à un événement (click, keypress, mouseenter etc etc)
@@ -29,14 +11,34 @@ $('#connexion').on('click', () => {
     const login = $('#login').val();
     const mdp = $('#password').val();
 
-    console.log('login', login);
-    console.log('mdp', mdp);
-
     //comparaison de l'input avec les bon credentials
     if (login === 'admin' && mdp === 'admin') {
-        window.location.href = "http://www.kana.fr/top-15-des-waifus/"; //lol
+
+        // Pour gérer le 'remember me', il faut utiliser sois le localStorage (pas de péremption) soit le sessionStorage (tout est effacé quand le mec quitte le site)
+        if ($('#remember').is(':checked')) {
+            localStorage.setItem("username", login);
+            localStorage.setItem("password", mdp);
+            
+            sessionStorage.removeItem("username");
+            sessionStorage.removeItem("password");
+            
+        } else {
+            localStorage.removeItem("username");
+            localStorage.removeItem("password");
+
+            sessionStorage.setItem("username", login);
+            sessionStorage.setItem("password", mdp);
+
+        }
+        window.location.href = "hub.html";
     }
     else {
-        window.alert("NAN C'EST PAS CA");;
+        $('.alert.alert-danger').removeClass('d-none');
     }
 });
+
+// Si on les infos dans le localStorage OU dans le sessionStorage
+if (localStorage.getItem("username") != null || sessionStorage.getItem("username") != null) {
+    window.location.href = "hub.html";        
+}
+
